66bc0e7c692b25b5feea0cdb94d6683a2d8d233d
VO Quoc An <hvcgtraining@gmail.com>
2020-06-21 00:17:47 +0700

    added add student feature + small modify in FaceDetection.py

--- src/FaceDetection.py
+++ src/FaceDetection.py
@@ -22,9 +22,9 @@ class FaceDetection:
 
     def draw_text(self, image, text, frame):
         (x, y, _, h) = frame
-        center = (x, int(y + h / 2))
+        center = (x, int(y + h + 60 / 2))
         font = cv2.FONT_HERSHEY_SIMPLEX
-        cv2.putText(image, text, center, font, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
+        cv2.putText(image, text, center, font, 0.8, (0, 255 , 0), 2, cv2.LINE_AA)
 
     def open(self):
         cv2.namedWindow("acd_face")
@@ -41,6 +41,8 @@ class FaceDetection:
             
             cv2.imshow("acd_face", frame)
             cv2.waitKey(1)
+            
+    
 
     def detect_faces(self, image):
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -62,6 +64,27 @@ class FaceDetection:
             self.draw_rectangle(image, (x, y, w, h))
             
         return image, faces
+    
+    def detect_faces2(self, image):
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        coord_faces = self.face_cascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
+            flags = cv2.CASCADE_SCALE_IMAGE
+        )
+        
+        faces = []
+        temp = 0
+        for (x, y, w, h) in coord_faces:
+            temp = image[y:y+h, x:x+w]
+            faces.append(temp)
+            pred = self.model.predict(temp)
+            self.draw_text(image, self.classes[pred], (x, y, w, h))
+            self.draw_rectangle(image, (x, y, w, h))
+            
+        return image, temp
 
     def save_img(self, face):
         imgname = "sample/" + str(self.img_index) + ".png"
@@ -70,3 +93,35 @@ class FaceDetection:
         cv2.imwrite(imgname, face)
         cv2.imwrite(imgname, face)
 
+
+    def open2_face(self):
+        cv2.namedWindow("acd_face")
+        index = 0
+        while True:
+            ret, frame = self.cam.read()
+
+            if not ret:
+                break
+
+            image, faces = self.detect_faces2(frame)
+
+            
+            k = cv2.waitKey(1)
+            if k%256 == 27:
+                # ESC pressed
+                print("Escape hit, closing...")
+                break
+            elif k%256 == 32:
+                #imgname = str(datetime.datetime.now()) + ".png"
+                imgname = "sample/" + str(index) + ".png"
+                index += 1
+                print("Save new image " + imgname)
+                print(faces)
+                cv2.imwrite(imgname, faces)
+                print(cv2.imwrite(imgname, faces))
+
+            if (cv2.getWindowProperty("acd_face", 1) == -1):
+                break
+            
+            cv2.imshow("acd_face", frame)
+            cv2.waitKey(1)
